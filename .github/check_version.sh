b3a902da1f81541ac2878c4836102413546692a2
Niko Diamadis <niko@cyb3rko.de>
2025-02-09 17:23:53 +0100

    Improve verbosity of version check

--- .github/check_version.sh
+++ .github/check_version.sh
@@ -1,8 +1,14 @@
-hash=$(docker images --no-trunc --quiet $REGISTRY/$IMAGE_NAME:latest)
+old_hash=$(cat .image)
+new_hash=$(docker images --no-trunc --quiet $REGISTRY/$IMAGE_NAME:latest)
+echo "Previous image hash:"
+echo $old_hash
 echo "New image hash:"
-echo $hash
+echo $new_hash
 
-if [ ! -f ".image" ] || [ $hash != $(cat .image) ]; then
+if [ ! -f ".image" ] || [ $new_hash != $old_hash ]; then
+  echo "New image found, requesting image push to registry"
   echo "update=true" >> $GITHUB_OUTPUT
+else
+  echo "Image has not changed, no image push to registry required"
 fi
 
