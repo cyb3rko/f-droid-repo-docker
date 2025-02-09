e54e51d54378dc5024deeceee5d38edc5f1ef641
Niko Diamadis <niko@cyb3rko.de>
2025-02-09 17:08:04 +0100

    Fix CI permissions and replace hard-coded names

--- .github/check_version.sh
+++ .github/check_version.sh
@@ -1,7 +1,8 @@
+hash=$(docker images --no-trunc --quiet ${{ env.REGISTRY }}/${{ env.IMAGE_NAME }}:latest)
 echo "New image hash:"
-echo $(docker images --no-trunc --quiet f-droid-repo:latest)
+echo $hash
 
-if [ ! -f ".image" ] || [ $(docker images --no-trunc --quiet f-droid-repo:latest) != $(cat .image) ]; then
+if [ ! -f ".image" ] || [ $hash != $(cat .image) ]; then
   echo "update=true" >> $GITHUB_OUTPUT
 fi
 
